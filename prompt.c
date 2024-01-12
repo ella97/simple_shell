98054153a00ca5f7b7ee23770ee55155ef4d48d9
ella97 <nuwymwanri@gmail.com>
2024-01-12 22:51:02 +0300

    .\prompt.c

--- prompt.c
+++ prompt.c
@@ -8,16 +8,16 @@
  */
 void _prompt(int mode, char **line, alias_t **ls)
 {
-size_t line_cap = 0;
-int line_length = 0;
-if (mode == 1)
-write(1, "$ ", 2);
-line_length = _get_line(line, &line_cap, stdin);
-if (line_length == -1)
-{
-_free_alias(ls);
-exit(errno);
-}
+    size_t line_cap = 0;
+    int line_length = 0;
+    if (mode == 1)
+        write(1, "$ ", 2);
+    line_length = _get_line(line, &line_cap, stdin);
+    if (line_length == -1)
+    {
+        _free_alias(ls);
+        exit(errno);
+    }
 }
 
 /**
@@ -26,49 +26,49 @@ exit(errno);
  * @line_cap: Line limit
  * @file_stream: File stream
  * Return: Length of line
-*/
+ */
 
 int _get_line(char **line_ptr, size_t *line_cap, FILE *file_stream)
 {
-char ch = '0', *str;
-ssize_t fd, st;
-static size_t val;
+    char ch = '0', *str;
+    ssize_t fd, st;
+    static size_t val;
 
-if (line_ptr == NULL || line_cap == NULL)
-{
-errno = EINVAL;
-return (-1);
-}
+    if (line_ptr == NULL || line_cap == NULL)
+    {
+        errno = EINVAL;
+        return (-1);
+    }
 
-str = malloc((*line_cap = *line_cap + 1) * sizeof(char));
-if (!str || val != 0)
-return (-1);
-fflush(file_stream);
+    str = malloc((*line_cap = *line_cap + 1) * sizeof(char));
+    if (!str || val != 0)
+        return (-1);
+    fflush(file_stream);
 
-for (; ch != '\n'; val++)
-{
-fd = read(STDIN_FILENO, &ch, 1);
-if (fd == -1 || (fd == 0 && val == 0))
-{
-free(str);
-return (-1);
-}
-str[val] = ch;
-if (val == (*line_cap - 1))
-{
-str = _realloc(str, *line_cap, (*line_cap + 2));
-*line_cap += 2;
-}
-}
-str[val] = '\n';
-str[val++] = '\0';
-*line_ptr = str;
+    for (; ch != '\n'; val++)
+    {
+        fd = read(STDIN_FILENO, &ch, 1);
+        if (fd == -1 || (fd == 0 && val == 0))
+        {
+            free(str);
+            return (-1);
+        }
+        str[val] = ch;
+        if (val == (*line_cap - 1))
+        {
+            str = _realloc(str, *line_cap, (*line_cap + 2));
+            *line_cap += 2;
+        }
+    }
+    str[val] = '\n';
+    str[val++] = '\0';
+    *line_ptr = str;
 
-st = val;
-if (st != 0)
-val = 0;
+    st = val;
+    if (st != 0)
+        val = 0;
 
-return (st);
+    return (st);
 }
 
 /**
@@ -80,37 +80,37 @@ return (st);
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *ar, *tmp;
-unsigned int i;
+    char *ar, *tmp;
+    unsigned int i;
 
-if (new_size == old_size)
-return (ptr);
+    if (new_size == old_size)
+        return (ptr);
 
-if (new_size == 0 && ptr)
-{
-free(ptr);
-return (NULL);
-}
+    if (new_size == 0 && ptr)
+    {
+        free(ptr);
+        return (NULL);
+    }
 
-if (ptr == NULL)
-return (malloc(new_size));
+    if (ptr == NULL)
+        return (malloc(new_size));
 
-ar = malloc(new_size);
-if (!ar)
-return (NULL);
+    ar = malloc(new_size);
+    if (!ar)
+        return (NULL);
 
-tmp = ptr;
+    tmp = ptr;
 
-if (new_size > old_size)
-for (i = 0; i < old_size; i++)
-ar[i] = tmp[i];
+    if (new_size > old_size)
+        for (i = 0; i < old_size; i++)
+            ar[i] = tmp[i];
 
-if (new_size < old_size)
-for (i = 0; i < new_size; i++)
-ar[i] = tmp[i];
+    if (new_size < old_size)
+        for (i = 0; i < new_size; i++)
+            ar[i] = tmp[i];
 
-free(ptr);
-return (ar);
+    free(ptr);
+    return (ar);
 }
 
 /**
@@ -121,17 +121,17 @@ return (ar);
 
 void _free_alias(alias_t **head)
 {
-alias_t *nextNode, *currentNode;
-if (head == NULL)
-return;
-currentNode = *head;
-while (currentNode)
-{
-nextNode = currentNode->next;
-free(currentNode->name);
-free(currentNode->val);
-free(currentNode);
-currentNode = nextNode;
-}
-*head = NULL;
+    alias_t *nextNode, *currentNode;
+    if (head == NULL)
+        return;
+    currentNode = *head;
+    while (currentNode)
+    {
+        nextNode = currentNode->next;
+        free(currentNode->name);
+        free(currentNode->val);
+        free(currentNode);
+        currentNode = nextNode;
+    }
+    *head = NULL;
 }
--- shell.c
+++ shell.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* @argc: Argument count
-* @argv: List of arguments
-* Return: Integer
-**/
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: List of arguments
+ * Return: Integer
+ **/
 
 int main(attribute((unused)) int argc, char argv)
 {
-char line[100000], dynamic = NULL;
-int status = isatty(STDIN_FILENO), attempt = 0;
-char **text = NULL, sep = " \n";
-alias_t *aliasL = NULL;
+    char line[100000], dynamic = NULL;
+    int status = isatty(STDIN_FILENO), attempt = 0;
+    char **text = NULL, sep = " \n";
+    alias_t *aliasL = NULL;
 
-errno = 0;
-while (1 == 1)
-{
-attempt++;
-_prompt(status, &dynamic, &aliasL);
-_strcpy(line, dynamic);
-free(dynamic);
+    errno = 0;
+    while (1 == 1)
+    {
+        attempt++;
+        _prompt(status, &dynamic, &aliasL);
+        _strcpy(line, dynamic);
+        free(dynamic);
 
-_remove_comments(line);
-text = _tokenizer(line, sep);
-if (text[0] == NULL)
-{
-free(text);
-continue;
-}
-_prepare(text, attempt, argv, &aliasL);
+        _remove_comments(line);
+        text = _tokenizer(line, sep);
+        if (text[0] == NULL)
+        {
+            free(text);
+            continue;
+        }
+        _prepare(text, attempt, argv, &aliasL);
 
-free(text);
-}
+        free(text);
+    }
 
-return (errno);
+    return (errno);
 }
